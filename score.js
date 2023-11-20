0c57790dda534f0977c8563559a2a9213d1be41f
kimia <kshahbaghi@gmail.com>
2023-11-20 20:50:53 +0330

    solve players rank problem

---
+++ score.js
@@ -0,0 +1,26 @@
+function playerRanks(ranks, scores) {
+  const uniqueRanks = [...new Set(ranks)];
+  let result = [];
+  for (let score of scores) {
+    if (score >= uniqueRanks[0]) {
+      result.push(1);
+    } else if (score < uniqueRanks[uniqueRanks.length - 1]) {
+      result.push(ranks.length + 1);
+    } else {
+      for (let i = 1; i < uniqueRanks.length; i++) {
+        if (uniqueRanks[i - 1] > score && score >= uniqueRanks[i]) {
+          result.push(i + 1);
+          break;
+        }
+      }
+    }
+  }
+  return result;
+}
+
+const result = playerRanks(
+  [100, 90, 90, 80, 75, 60, 30, 10],
+  [120, 50, 65, 77, 90]
+);
+
+console.log({ result });
