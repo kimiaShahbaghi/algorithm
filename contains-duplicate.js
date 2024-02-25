1846604709f031cb69a7e3e69f7b5262d0727143
kimiaShahbaghi <39163453+kimiaShahbaghi@users.noreply.github.com>
2024-02-25 17:50:05 +0330

    contains duplicate

--- contains-duplicate.js
+++ contains-duplicate.js
@@ -11,7 +11,6 @@ const containsDuplicate = (array) => {
 const containsDuplicatee = (nums) => {
   for (let i = 0; i < nums.length; i++) {
     for (let j = i + 1; j < nums.length; j++) {
-      console.log(i, j, nums[i], nums[j]);
       if (nums[i] === nums[j]) {
         return true;
       }
